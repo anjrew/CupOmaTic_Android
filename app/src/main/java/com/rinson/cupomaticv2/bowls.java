53493e4da59a696f473272090c4f6e92801de09a
“earyzhe” <earyzhe@gmail.com>
2018-04-06 15:36:59 +0200

    6.4.2018#3
    Getting links down. now between main activity and bowls

--- app/src/main/java/com/rinson/cupomaticv2/bowls.java
+++ app/src/main/java/com/rinson/cupomaticv2/bowls.java
@@ -1,7 +1,10 @@
 package com.rinson.cupomaticv2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class bowls extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class bowls extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bowls);
+    }
+
+    public void startButton(View view) {
+        openBowlsActivity();
+        Log.i("Info", "Start Button pressed");
+    }
+
+    public void openBowlsActivity() {
+
+        Intent intent = new Intent(bowls.this, MainActivity.class);
+
+        startActivity(intent);
     }
 }
