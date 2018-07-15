6eb505bb776d478b9d5ef43a57721e8685a808a0
“earyzhe” <earyzhe@gmail.com>
2018-07-15 17:22:48 +0200

    15.7.2018 - 17:22
    got the round descriptions in there

--- app/src/main/java/com/rinson/cupomaticv2/Help.java
+++ app/src/main/java/com/rinson/cupomaticv2/Help.java
@@ -12,18 +12,28 @@ public class Help extends AppCompatActivity {
     private String[] item;
     private String[] description;
     boolean checked;
+    public String advancedModeText;
+    public String voicePromptsText;
+    public String vibrateText;
+    public String mainTimerText;
+    public String intervalText;
+    public String breakText;
+    public String sampleText;
+    public String roundsText;
+    public String contactText;
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+        setupDescriptionStrings();
         setUpValues();
         createListView();
     }
 
     public void setUpValues(){
-        item  =  new String[]{"Advanced mode", "Voice prompts", "Vibrate", "Main Timer", "Interval", "Break", "Sample", "Round one", "Round two", "Round three", "Contact"};
-        description = new String[]{"Advanced mode", "Voice prompts", "Vibrate","Main Timer", "Interval", "Break", "Sample", "Round one", "Round two", "Round three", "Contact"};
+        item  =  new String[]{"Advanced mode", "Voice prompts", "Vibrate", "Main Timer", "Interval", "Break", "Sample", "Rounds", "Contact"};
+        description = new String[]{advancedModeText, voicePromptsText, vibrateText,mainTimerText, intervalText, breakText, sampleText, roundsText, contactText};
     }
 
 
@@ -41,5 +51,15 @@ public class Help extends AppCompatActivity {
         });
     }
 
-
+    public void setupDescriptionStrings(){
+        advancedModeText = (String)getText(R.string.Advanced_mode_description);
+        voicePromptsText = (String)getText(R.string.Voice_prompts_description);
+        vibrateText = (String)getText(R.string.Vibrate_description);
+        mainTimerText = (String)getText(R.string.Main_timer_description);
+        intervalText = (String)getText(R.string.Interval_description);
+        breakText = (String)getText(R.string.Break_description);
+        sampleText = (String)getText(R.string.Sample_description);
+        roundsText = (String) getText(R.string.Rounds_description);
+        contactText = (String) getText(R.string.Contact_description);
+    }
 }
